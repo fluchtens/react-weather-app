54aacfc3b675f6e65ceac3525c4866fce4ede864
Cleazyy <cleazy2002@gmail.com>
2023-09-30 11:49:58 +0200

    add forecast to interface

--- frontend/src/utils/weather.interface.tsx
+++ frontend/src/utils/weather.interface.tsx
@@ -1,47 +1,41 @@
 export interface Weather {
-  current: Current;
   location: Location;
+  current: Current;
+  forecast: Forecast;
+}
+
+interface Location {
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  localtime: string;
 }
 
 interface Current {
-  cloud: number;
-  feelslike_c: number;
-  feelslike_f: number;
-  gust_kph: number;
-  gust_mph: number;
-  humidity: number;
-  is_day: boolean;
-  last_updated: string;
-  last_updated_epoch: number;
-  precip_in: number;
-  precip_mm: number;
-  pressure_in: number;
-  pressure_mb: number;
   temp_c: number;
   temp_f: number;
-  uv: number;
-  vis_km: number;
-  vis_miles: number;
-  wind_degree: number;
-  wind_dir: string;
-  wind_kph: number;
-  wind_mph: number;
+  feelslike_c: number;
+  feelslike_f: number;
   condition: Condition;
 }
 
 interface Condition {
-  code: number;
   icon: string;
   text: string;
 }
 
-interface Location {
-  country: string;
-  lat: number;
-  localtime: string;
-  localtime_epoch: number;
-  lon: number;
-  name: string;
-  region: string;
-  tz_id: string;
+interface Forecast {
+  forecastday: ForecastDay[];
+}
+
+interface ForecastDay {
+  hour: Hour[];
+}
+
+interface Hour {
+  temp_c: number;
+  temp_f: number;
+  condition: Condition;
 }
