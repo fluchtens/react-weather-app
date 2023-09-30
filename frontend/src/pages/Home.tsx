970c1c00c670f7726ed7ab2cb8a4053a165952a6
Cleazyy <cleazy2002@gmail.com>
2023-09-30 11:36:37 +0200

    create today's forecast list

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,28 +4,58 @@ import { Weather } from "../utils/weather.interface";
 import styles from "../styles/Home.module.scss";
 
 function Home() {
-  // const [weather, setWeather] = useState<Weather | null | undefined>(null);
+  const [weather, setWeather] = useState<Weather | null | undefined>(null);
+  const forecastHours = [6, 9, 12, 15, 18, 21];
 
-  // useEffect(() => {
-  //   async function getWeather() {
-  //     try {
-  //       const data = await getCityWeather("Marche-lez-Ecausinnes");
-  //       setWeather(data);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
+  useEffect(() => {
+    async function getWeather() {
+      try {
+        const data = await getCityWeather("Marche-lez-Ecausinnes");
+        setWeather(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
-  //   getWeather();
-  // }, []);
+    getWeather();
+  }, []);
 
   return (
     <main className={styles.main}>
-      <h1>Brussels</h1>
-      <h1>16°</h1>
-      <p>Feels like 15°</p>
-      <img src="https://cdn.weatherapi.com/weather/64x64/day/296.png" />
-      <p>Light rain</p>
+      <h1 className={styles.city}>{weather?.location.name}</h1>
+      <div className={styles.tempContainer}>
+        <h2 className={styles.temp}>{weather?.current.temp_c}°</h2>
+        <p className={styles.feelsLike}>
+          Feels like {weather?.current.feelslike_c}°
+        </p>
+        <img src={weather?.current.condition.icon} />
+        <p className={styles.feelsLike}>{weather?.current.condition.text}</p>
+      </div>
+
+      <div className={styles.todayForecast}>
+        <p className={styles.todayForecastTitle}>Today's Forecast</p>
+        <ul className={styles.todayForecastList}>
+          {forecastHours.map((hour) => {
+            const displayHour = hour > 12 ? hour - 12 : hour;
+            const displayPeriod = hour >= 12 ? "PM" : "AM";
+            return (
+              <li key={hour}>
+                <p
+                  className={styles.forecastHour}
+                >{`${displayHour}:00 ${displayPeriod}`}</p>
+                <img
+                  src={
+                    weather?.forecast.forecastday[0].hour[hour].condition.icon
+                  }
+                />
+                <p className={styles.forecastTemp}>
+                  {weather?.forecast.forecastday[0].hour[hour].temp_c}°
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </main>
   );
 }
