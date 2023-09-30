469f557bc8dcaa73498c56d910ac0ea784711edd
Cleazyy <cleazy2002@gmail.com>
2023-09-30 14:05:14 +0200

    add type to current interface

--- frontend/src/utils/current.interface.tsx
+++ frontend/src/utils/current.interface.tsx
@@ -1,9 +1,13 @@
 import { Condition } from "./condition.interface";
 
 export interface Current {
+  condition: Condition;
   temp_c: number;
   temp_f: number;
   feelslike_c: number;
   feelslike_f: number;
-  condition: Condition;
+  wind_kph: number;
+  wind_mph: number;
+  humidity: number;
+  uv: number;
 }
