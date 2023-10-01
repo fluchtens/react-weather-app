fb246d8bc94687977acb5187f7831c57cc1fadba
Cleazyy <cleazy2002@gmail.com>
2023-10-01 13:07:25 +0200

    init city search bar

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,22 +8,43 @@ import CurrentWeather from "../components/CurrentWeather";
 
 function Home() {
   const [weather, setWeather] = useState<Weather | null | undefined>(null);
+  const [city, setCity] = useState("Bruxelles");
+  const [trigger, setTrigger] = useState(true);
 
   useEffect(() => {
     async function getWeather() {
       try {
-        const data = await getCityWeather("Marche-lez-ecaussinnes");
-        setWeather(data);
+        if (city && trigger) {
+          const data = await getCityWeather(city);
+          setWeather(data);
+          setCity("");
+        }
       } catch (error) {
         console.error(error);
       }
     }
-
     getWeather();
-  }, []);
+    setTrigger(false);
+  }, [city, trigger]);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === "Enter") {
+      setTrigger(true);
+    }
+  };
 
   return (
     <main className={styles.main}>
+      <div className={styles.container}>
+        <input
+          className={styles.searchBar}
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyPress={handleKeyPress}
+          placeholder="Search a city"
+        />
+      </div>
       <CurrentWeather weather={weather} />
       <TodayForecast weather={weather} />
       <CurrentDetails weather={weather} />
