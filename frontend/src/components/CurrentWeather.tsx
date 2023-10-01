95fd8def53cc1f71ed816f9c40e61b30d97af170
Cleazyy <cleazy2002@gmail.com>
2023-10-01 17:38:26 +0200

    update current weather component

--- frontend/src/components/CurrentWeather.tsx
+++ frontend/src/components/CurrentWeather.tsx
@@ -4,8 +4,20 @@ import styles from "../styles/CurrentWeather.module.scss";
 function CurrentWeather({ weather }: { weather: Weather | null | undefined }) {
   return (
     <div className={styles.container}>
-      <h1 className={styles.city}>{weather?.location.name}</h1>
-      <h2 className={styles.temp}>{weather?.current.temp_c}°</h2>
+      <div className={styles.location}>
+        <p className={styles.city}>{weather?.location.name}</p>
+        {weather?.location.region ? (
+          <p className={styles.country}>
+            {weather?.location.country} ({weather?.location.region})
+          </p>
+        ) : (
+          <p className={styles.country}>{weather?.location.country}</p>
+        )}
+      </div>
+      <div className={styles.condition}>
+        <img className={styles.icon} src={weather?.current.condition.icon} />
+        <p className={styles.temp}>{weather?.current.temp_c}°</p>
+      </div>
     </div>
   );
 }
